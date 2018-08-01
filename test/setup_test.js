1a7e2f285a5ca5764f3184be612beb3ef3241281
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2018-08-01 16:45:12 -0500

    Removing async

--- test/setup_test.js
+++ test/setup_test.js
@@ -7,9 +7,9 @@ before(async () => {
   global.accounts = await web3.eth.personal.getAccounts();
   global.assert = assert;
 
-  it('should connect to web3', async () => {
+  it('should connect to web3', () => {
     assert.equal(accounts.length, 10, "There should be 10 ETH accounts.")
-  })
+  });
 
   it('should be network 50', async () => {
     assert.equal(await web3.eth.net.getId(), 50, "The Contract test rpc should be running.  TODO: should discuss moving it into a mono repo");
