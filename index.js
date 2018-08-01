b9610fcc99fb1b791cecb891053804268193a8a1
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2018-08-01 14:48:38 -0500

    Mergaing in Paradigm.js

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const Bank = require('./lib/bank');
 const Web3 = require('web3');
 const OrderGateway = require('./lib/OrderGateway');
+const Order = require('./lib/Order');
 
-const ParadigmJS = class {
+class Paradigm {
   constructor(options) {
     this.web3 = new Web3(options.provider || 'default_provider_code');
     this.bank = new Bank(this.web3);
-    this.OrderGateway = new OrderGateway(this.web3, options.networkId );
+    this.OrderGateway = new OrderGateway(this.web3, options.networkId);
+    Order.prototype.web3 = this.web3;
+    Order.prototype.OrderGateway = this.OrderGateway;
+    this.Order = Order;
   }
 };
 
 
-ParadigmJS.utils = require('./lib/utils');
-ParadigmJS.Bank = Bank;
-ParadigmJS.messages = require('./lib/messages');
+Paradigm.utils = require('./lib/utils');
+Paradigm.Bank = Bank;
+Paradigm.messages = require('./lib/messages');
 
-module.exports = ParadigmJS;
+module.exports = Paradigm;
