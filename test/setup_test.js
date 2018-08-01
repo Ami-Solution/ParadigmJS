a18e47c8be4c7a57cf406ecfa383d384e4296945
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2018-08-01 14:48:59 -0500

    exposing assert globally

--- test/setup_test.js
+++ test/setup_test.js
@@ -5,6 +5,7 @@ before(async () => {
 
   global.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
   global.accounts = await web3.eth.personal.getAccounts();
+  global.assert = assert;
 
   it('should connect to web3', async () => {
     assert.equal(accounts.length, 10, "There should be 10 ETH accounts.")
