3d8a4cfb4b6f20b0a201c58d4855b016d5c9e5c6
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2018-08-01 12:22:44 -0500

    Adding a placeholder test for participate.

--- test/order_gateway_test.js
+++ test/order_gateway_test.js
@@ -15,10 +15,12 @@ describe('OrderGateway', () => {
   it('should get the makerDataTypes of a SubContract', async () => {
     const makerDataTypes = await OrderGateway.makerDataTypes(basicTradeSubContractAddress);
     assert.doesNotThrow(() => { JSON.parse(makerDataTypes) });
-  })
+  });
 
   it('should get the takerDataTypes of a SubContract', async () => {
     const takerDataTypes = await OrderGateway.takerDataTypes(basicTradeSubContractAddress);
     assert.doesNotThrow(() => { JSON.parse(takerDataTypes) });
-  })
+  });
+
+  it('should participate in a fully constructed Order.');
 });
