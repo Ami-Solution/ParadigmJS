4539ba9e200e24ccb3482c7711a24e9cabdba815
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2018-08-01 12:18:52 -0500

    Adding OrderGateway and a couple of simple tests for it.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const Bank = require('./lib/bank');
 const Web3 = require('web3');
+const OrderGateway = require('./lib/OrderGateway');
 
 const ParadigmJS = class {
   constructor(options) {
     this.web3 = new Web3(options.provider || 'default_provider_code');
     this.bank = new Bank(this.web3);
+    this.OrderGateway = new OrderGateway(this.web3, options.networkId );
   }
 };
 
---
+++ test/order_gateway_test.js
@@ -0,0 +1,24 @@
+const assert = require('assert');
+const ParadigmJS = require('../index');
+const BasicTradeSubContract = require('../lib/contracts/BasicTradeSubContract');
+
+
+describe('OrderGateway', () => {
+  let paradigm, OrderGateway;
+  const basicTradeSubContractAddress = BasicTradeSubContract.networks[50].address;
+
+  before(async () => {
+    paradigm = new ParadigmJS({ provider: web3.currentProvider, networkId: await web3.eth.net.getId() });
+    OrderGateway = paradigm.OrderGateway;
+  });
+
+  it('should get the makerDataTypes of a SubContract', async () => {
+    const makerDataTypes = await OrderGateway.makerDataTypes(basicTradeSubContractAddress);
+    assert.doesNotThrow(() => { JSON.parse(makerDataTypes) });
+  })
+
+  it('should get the takerDataTypes of a SubContract', async () => {
+    const takerDataTypes = await OrderGateway.takerDataTypes(basicTradeSubContractAddress);
+    assert.doesNotThrow(() => { JSON.parse(takerDataTypes) });
+  })
+});
