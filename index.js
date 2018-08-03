f0a2b6e617eda8197d37758bab5b3cfea29bfc70
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2018-08-03 12:52:29 -0500

    Supplying bank with the gateway

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ class Paradigm {
     this.web3 = new Web3(options.provider || 'default_provider_code');
     let endpoint                 = options.orderStreamURL || 'http://osd.paradigm.market:3000';
     this.orderStream             = new OrderStream(endpoint);
-    this.bank = new Bank(this.web3);
     this.orderGateway = new OrderGateway(this.web3, options.networkId);
+    this.bank = new Bank(this.web3, this.orderGateway);
     Order.prototype.web3 = this.web3;
     Order.prototype.orderGateway = this.orderGateway;
     this.Order = Order;
