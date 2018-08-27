3482b359b62c0499fd06ff65cb813cd57002b07b
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2018-08-27 15:16:29 -0500

    moving paradigm init to root setup file.

--- test/setup_test.js
+++ test/setup_test.js
@@ -1,6 +1,7 @@
 const Web3 = require('web3');
 const chai = require('chai');
 const ganacheProvider = require('ganache-core').provider();
+const Paradigm = require('../index');
 
 const tokenHelper = require('./helpers/tokenHelper.js');
 const paradigmContractHelper = require('./helpers/paradigmContractHelper');
@@ -12,8 +13,9 @@ before(async () => {
   chai.should();
   global.web3 = new Web3(ganacheProvider);
   global.accounts = await web3.eth.personal.getAccounts();
+  global.paradigm = new Paradigm({ provider: web3.currentProvider, networkId: await web3.eth.net.getId() });
 
-  await paradigmContractHelper();
+  // await paradigmContractHelper();
 
   await tokenHelper('TKA', 'Token A', 'TKA', accounts[7]);
   await tokenHelper('TKB', 'Token B', 'TKB', accounts[8]);
